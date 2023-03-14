54dd3652e7dcb30ec0a5f4623307e4c26ee7203e
Boldolsky <boldab24@gmail.com>
2023-03-14 07:12:02 -0700

    Libmy

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *  _atoi - check the description
+ *  @s: takes a strong as input
+ *  description: converts string digit to int
+ *  Return: int
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
+
