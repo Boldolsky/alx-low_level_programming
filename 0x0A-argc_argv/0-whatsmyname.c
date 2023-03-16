c8ac2721ee882e9abdd70fae9c5ae4aac5c0c645
Boldolsky <boldab24@gmail.com>
2023-03-15 19:25:38 -0700

    Myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: command line argument
+ * @argv: command line argument
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+argc simply means to count, it counts the number of characters in a code WHILE argv simply means vector, it stores character in an arrray.
